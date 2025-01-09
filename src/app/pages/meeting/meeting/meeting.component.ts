c561b05077c0a692fcc3c91bab4a9a62dea889b0
Jamike <jamikeobikwelu@gmail.com>
2025-01-09 23:41:13 +0100

    Latest

--- src/app/pages/meeting/meeting/meeting.component.ts
+++ src/app/pages/meeting/meeting/meeting.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'app-meeting',
   templateUrl: './meeting.component.html',
   styleUrls: ['./meeting.component.css']
 })
-export class MeetingComponent {
+export class MeetingComponent implements AfterViewInit{
+
+  ngAfterViewInit(): void {
+ 
+    
+  }
 
 }
