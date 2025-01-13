6adc27eb2bd134a085e66368c46d1a823de19446
yungjoel <chibuzoronyedikachi72@gmail.com>
2025-01-13 16:57:02 +0100

    changes

--- src/app/shared/resueableComponents/header/header/header.component.ts
+++ src/app/shared/resueableComponents/header/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
 
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    const menuTrigger = this.elementRef.nativeElement.querySelector('.menu-trigger');
+    const nav = this.elementRef.nativeElement.querySelector('.nav');
+
+    if (menuTrigger && nav) {
+      menuTrigger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+        menuTrigger.classList.toggle('active');
+      });
+    }
+  }
 }
